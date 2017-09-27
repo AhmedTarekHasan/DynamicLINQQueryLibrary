fdcc236e6aced9eead9ea9182e44fb2d7d2a39c0
Ahmed Tarek Hasan <eng_ahmed.tarek@hotmail.com>
2017-09-28 00:37:27 +0200

    Add files via upload

---
+++ DynamicLinqExtensionMethods/ExtensionMethods.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace DynamicLinqExtensionMethodsNS
+{
+    public static class ExtensionMethods
+    {
+        public static IQueryable<T> Where<T>(this IQueryable<T> Caller, OringGroup oringGroup)
+        {
+            Type ItemsType = typeof(T);
+            ParameterExpression ItemTypeParameterExpression = Expression.Parameter(ItemsType, "paramDummyName");
+
+            Expression OverAllExpression = GetCondition<T>(oringGroup, ItemTypeParameterExpression);
+            return Caller.Where(Expression.Lambda<Func<T, bool>>(OverAllExpression, ItemTypeParameterExpression));
+        }
+
+        public static IQueryable<T> WhereNot<T>(this IQueryable<T> Caller, OringGroup oringGroup)
+        {
+            Type ItemsType = typeof(T);
+            ParameterExpression ItemTypeParameterExpression = Expression.Parameter(ItemsType, "paramDummyName");
+
+            Expression OverAllExpression = GetCondition<T>(oringGroup, ItemTypeParameterExpression);
+            OverAllExpression = Expression.Not(OverAllExpression);
+            return Caller.Where(Expression.Lambda<Func<T, bool>>(OverAllExpression, ItemTypeParameterExpression));
+        }
+
+        static Expression GetCondition<T>(OringGroup oringGroup, ParameterExpression ItemTypeParameterExpression)
+        {
+            if (oringGroup == null || oringGroup.Count == 0)
+            {
+                return null;
+            }
+
+            Expression OverAllExpression = MakeAndExpression<T>(oringGroup[0], ItemTypeParameterExpression);
+
+            if (oringGroup.Count > 1)
+            {
+                for (var i = 1; i < oringGroup.Count; i++)
+                {
+                    if (oringGroup[i] == null || oringGroup[i].Count == 0)
+                    {
+                        continue;
+                    }
+
+                    OverAllExpression = Expression.Or(OverAllExpression, MakeAndExpression<T>(oringGroup[i], ItemTypeParameterExpression));
+                }
+            }
+
+            return OverAllExpression;
+        }
+
+        static Expression MakeAndExpression<T>(AndingGroup andingGroup, ParameterExpression myParameterExpression)
+        {
+            if (andingGroup == null || andingGroup.Count == 0)
+            {
+                return null;
+            }
+
+            Type ItemsType = typeof(T);
+            PropertyInfo FirstProperty = ItemsType.GetProperty(andingGroup[0].PropertyName);
+
+            MemberExpression PropertyAccess = Expression.MakeMemberAccess(myParameterExpression, FirstProperty);
+            ConstantExpression FirstConstantValue = Expression.Constant(andingGroup[0].Value);
+
+            Expression OverallCondition = null;
+
+
+            if (Enum.IsDefined(typeof(System.Linq.Expressions.ExpressionType), andingGroup[0].ComparisonOperator.ToString()))
+            {
+                System.Linq.Expressions.ExpressionType expType = (System.Linq.Expressions.ExpressionType)Enum.Parse(typeof(System.Linq.Expressions.ExpressionType), andingGroup[0].ComparisonOperator.ToString());
+                OverallCondition = Expression.MakeBinary(expType, PropertyAccess, FirstConstantValue);
+            }
+            else
+            {
+                switch (andingGroup[0].ComparisonOperator)
+                {
+                    case OperatorType.RegexMatch:
+                        OverallCondition = ExpressionExtension.RegexMatch(PropertyAccess, FirstConstantValue);
+                        break;
+                    case OperatorType.Contains:
+                        OverallCondition = ExpressionExtension.Contains(PropertyAccess, FirstConstantValue);
+                        break;
+                }
+            }
+
+            if (andingGroup.Count > 1)
+            {
+                for (int i = 1; i < andingGroup.Count; i++)
+                {
+                    PropertyInfo CurrentPoperty = ItemsType.GetProperty(andingGroup[i].PropertyName);
+                    MemberExpression CurrentPropertyAccess = Expression.MakeMemberAccess(myParameterExpression, CurrentPoperty);
+                    ConstantExpression CurrentConstantValue = Expression.Constant(andingGroup[i].Value);
+                    Expression CurrentCondition = null;
+                    
+                    if(Enum.IsDefined(typeof(System.Linq.Expressions.ExpressionType), andingGroup[i].ComparisonOperator.ToString()))
+                    {
+                        System.Linq.Expressions.ExpressionType expType = (System.Linq.Expressions.ExpressionType)Enum.Parse(typeof(System.Linq.Expressions.ExpressionType), andingGroup[i].ComparisonOperator.ToString());
+                        CurrentCondition = Expression.MakeBinary(expType, CurrentPropertyAccess, CurrentConstantValue);
+                    }
+                    else
+                    {
+                        switch (andingGroup[i].ComparisonOperator)
+                        {
+                            case OperatorType.RegexMatch:
+                                CurrentCondition = ExpressionExtension.RegexMatch(CurrentPropertyAccess, CurrentConstantValue);
+                                break;
+                            case OperatorType.Contains:
+                                CurrentCondition = ExpressionExtension.Contains(CurrentPropertyAccess, CurrentConstantValue);
+                                break;
+                        }
+                    }
+
+                    OverallCondition = Expression.And(OverallCondition, CurrentCondition);
+                }
+            }
+
+            return OverallCondition;
+        }
+    }
+
+    static class ExpressionExtension
+    {
+        public static Expression RegexMatch(Expression left, Expression right)
+        {
+            return Expression.Call(typeof(ExpressionExtensionMethodsImplementations).GetMethod("RegexMatch",
+            BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public)
+            , left, right);
+        }
+
+        public static Expression Contains(Expression left, Expression right)
+        {
+            return Expression.Call(typeof(ExpressionExtensionMethodsImplementations).GetMethod("Contains",
+            BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public)
+            , left, right);
+        }
+    }
+
+    static class ExpressionExtensionMethodsImplementations
+    {
+        static bool RegexMatch(string source, string pattern)
+        {
+            return Regex.IsMatch(source, pattern);
+        }
+
+        static bool Contains(string source, string pattern)
+        {
+            return source.Contains(pattern);
+        }
+    }
+
+    public class AndingItem
+    {
+        #region Fields & Properties
+        string propertyName;
+        public string PropertyName
+        {
+            get { return propertyName; }
+            set { propertyName = value; }
+        }
+
+        object value;
+        public object Value
+        {
+            get { return this.value; }
+            set { this.value = value; }
+        }
+
+        OperatorType comparisonOperator;
+        public OperatorType ComparisonOperator
+        {
+            get { return comparisonOperator; }
+            set { comparisonOperator = value; }
+        }
+        #endregion
+
+        #region Constructors
+        public AndingItem()
+        {
+        }
+        public AndingItem(string _propertyName, object _value, OperatorType _comparisonOperator)
+        {
+            PropertyName = _propertyName;
+            Value = _value;
+            ComparisonOperator = _comparisonOperator;
+        }
+        #endregion
+    }
+
+    public class AndingGroup : List<AndingItem>
+    {
+
+    }
+
+    public class OringGroup : List<AndingGroup>
+    {
+
+    }
+
+    public enum OperatorType
+    {
+        Equal = 1,
+        NotEqual = 2,
+        GreaterThan = 3,
+        GreaterThanOrEqual = 4,
+        LessThan = 5,
+        LessThanOrEqual = 6,
+        RegexMatch = 7,
+        Contains = 8
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Linq.Expressions;
+using System.Reflection;
+using DynamicLinqExtensionMethodsNS;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Order> lst = new List<Order>();
+	        lst.Add(new Order("a",1));
+	        lst.Add(new Order("b",4));
+	        lst.Add(new Order("a",5));
+	        lst.Add(new Order("a",5));
+	        lst.Add(new Order("c",7));
+	        lst.Add(new Order("d",9));
+	        lst.Add(new Order("a",14));
+	        lst.Add(new Order("a",2));
+	        lst.Add(new Order("a",5));
+            lst.Add(new Order("ahmed tarek hasan", 0));
+            lst.Add(new Order("mohamed emad eldin", -1));
+
+            OringGroup org = new OringGroup();
+
+            AndingGroup ang1 = new AndingGroup();
+            ang1.Add(new AndingItem(){ PropertyName = "Text", Value = "a", ComparisonOperator = OperatorType.Equal});
+            ang1.Add(new AndingItem(){ PropertyName = "Index", Value = 5, ComparisonOperator = OperatorType.Equal});
+
+            AndingGroup ang2 = new AndingGroup();
+            ang2.Add(new AndingItem() { PropertyName = "Text", Value = "b", ComparisonOperator = OperatorType.Equal });
+            ang2.Add(new AndingItem() { PropertyName = "Index", Value = 4, ComparisonOperator = OperatorType.Equal });
+
+            AndingGroup ang3 = new AndingGroup();
+            ang3.Add(new AndingItem() { PropertyName = "Index", Value = 7, ComparisonOperator = OperatorType.GreaterThanOrEqual });
+
+            AndingGroup ang4 = new AndingGroup();
+            ang4.Add(new AndingItem() { PropertyName = "Text", Value = "tarek", ComparisonOperator = OperatorType.RegexMatch });
+
+            AndingGroup ang5 = new AndingGroup();
+            ang5.Add(new AndingItem() { PropertyName = "Text", Value = "emad", ComparisonOperator = OperatorType.Contains });
+
+            org.Add(ang1);
+            org.Add(ang2);
+            org.Add(ang3);
+            org.Add(ang4);
+            org.Add(ang5);
+
+            var t = lst.AsQueryable().Where(org).ToList();
+			//var t = lst.AsQueryable().WhereNot(org).ToList();
+            dataGridView1.DataSource = t;
+        }
+
+    }
+
+    public class Order
+    {
+        string text;
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+
+        int index;
+        public int Index
+        {
+            get { return index; }
+            set { index = value; }
+        }
+
+        public Order()
+        {
+
+        }
+
+        public Order(string _text, int _index)
+        {
+            Text = _text;
+            Index = _index;
+        }
+    }
+}
